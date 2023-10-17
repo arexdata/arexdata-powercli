5da0284a7a88f262957a02962ba39df1a5968dfd
Alberto Gonzalez <agonzalez@arexdata.com>
2023-10-17 15:42:04 +0200

    skip cert core

--- Arexdata.psm1
+++ Arexdata.psm1
@@ -17,7 +17,13 @@ Get-ChildItem $psscriptroot\Modules\*.ps1 | % {
 	. $_.FullName
 }
 
-	# ignore certs for https
+# ignore certs for https
+if ($PSVersionTable.PSEdition -eq 'Core') {
+	# Invoke-restmethod provide Skip certcheck param in powershell core
+    $Script:PSDefaultParameterValues = @{
+    	"invoke-restmethod:SkipCertificateCheck" = $true
+        "invoke-webrequest:SkipCertificateCheck" = $true  }          
+} else {
 	add-type @"
 	    using System.Net;
 	    using System.Security.Cryptography.X509Certificates;
@@ -29,5 +35,7 @@ Get-ChildItem $psscriptroot\Modules\*.ps1 | % {
 	        }
 	    }
 "@
+
 	[System.Net.ServicePointManager]::CertificatePolicy = New-Object TrustAllCertsPolicy
 	[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Ssl3, [Net.SecurityProtocolType]::Tls, [Net.SecurityProtocolType]::Tls12
+}
